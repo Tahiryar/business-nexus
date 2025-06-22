e31030d80823f10cf2ff509e155a08afecb6f05f
Tahiryar <tahiryar.251k@gmail.com>
2025-06-22 22:49:53 +0500

    add sales schema and route for daily sales

--- business-nexus-backend/server.js
+++ business-nexus-backend/server.js
@@ -8,6 +8,7 @@ const authRoutes = require('./routes/authRoutes');
 const profileRoutes = require('./routes/profileRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const statsRoutes = require('./routes/statsRoutes');
+const saleRoutes = require('./routes/saleRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -24,7 +25,7 @@ app.use('/api', authRoutes);
 app.use('/api', profileRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/stats ', statsRoutes );
-
+app.use('/api/sales', saleRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
